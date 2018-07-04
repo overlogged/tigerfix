5263339abb1ce4f9d2aa7fc887edc4f512734d63
wtx1999 <771802026@qq.com>
2018-07-04 06:05:50 -0700

    wtx demo impletation

--- tfix/gen.py
+++ tfix/gen.py
@@ -2,36 +2,46 @@ import os
 import argparse
 from shutil import rmtree
 
-import elftools.elf.elffile as ef
+def hex_64bit(some_int):
+    ans = format(some_int, "x")
+    ans = ans.zfill(16) # 64 bit is 16 digits long in hex
+    return ans
 
-def main(args):
+def do_link(obj_files,target_file):
+    symbol_list = []
+    for file in obj_files:
+        libm_patch = lief.ELF.parse(file)
+        for x in libm_patch.relocations:
+            if x.symbol.name!='':
+                symbol_list.append("--defsym %s=0xc0ffee"%x.symbol.name)
 
-    # get parameters from args
-    main_path = args.main
-    patch_path = args.patch
-    target_path = args.object
+    command = "ld %s -shared -fno-plt %s -o %s" % (' '.join(obj_files),' '.join(symbol_list),target_file)
+    os.system(command)
+    # print(command)
+    return target_file
 
-    if (main_path is None) or (target_path is None):
-        raise AttributeError
 
-    # preparation
-    elf_file_patch = ef.ELFFile(open(patch_path, 'rb'))
-    elf_file_main = ef.ELFFile(open(main_path, 'rb'))
+def gen_config(main_path,so_path,config_path):
+    import elftools.elf.elffile as ef
+    a=open(so_path,'rb')
+    b=open(main_path,'rb')
+    #prepare
+    elffile_patch = ef.ELFFile(a)
+    elffile_main = ef.ELFFile(b)
 
     reladyn_name = '.rela.dyn'
     sym_name = '.symtab'
     relaplt_name = '.rela.plt'
     dynsym = '.dynsym'
 
-    reladyn_patch = elf_file_patch.get_section_by_name(reladyn_name)
-    symtab_patch = elf_file_patch.get_section_by_name(sym_name)
-    relaplt_patch = elf_file_patch.get_section_by_name(relaplt_name)
-    dynsym_patch = elf_file_patch.get_section_by_name(dynsym)
+    reladyn_patch = elffile_patch.get_section_by_name(reladyn_name)
+    symtab_patch = elffile_patch.get_section_by_name(sym_name)
+    relaplt_patch = elffile_patch.get_section_by_name(relaplt_name)
+    dynsym_patch = elffile_patch.get_section_by_name(dynsym)
 
-    symtab_main = elf_file_main.get_section_by_name(sym_name)
+    symtab_main = elffile_main.get_section_by_name(sym_name)
 
-
-    # first tab
+    #first tab
     extern_name = []
     main_exaddr = []
     got = []#(name ,addr)
@@ -58,7 +68,7 @@ def main(args):
             if(name_got==name):
                 got_addr.append(addr_got)
 
-    # second tab
+    #second tab
     oldfunc_name=[]
     patchfunc_addr=[]
     mainfunc_addr=[]
@@ -69,19 +79,57 @@ def main(args):
     for name in oldfunc_name:
         mainfunc_addr.append(symtab_main.get_symbol_by_name(name)[0].entry['st_value'])
 
-    # file type
-    if(elf_file_main.header['e_type']=='ET_EXEC'):
+    #file type
+    if(elffile_main.header['e_type']=='ET_EXEC'):
         sig=0
     else:
         sig=1
-        
-    # write
-    f=open(target_path, 'w+')
-    f.write('%d\n'%sig)
-    f.write("%d\n"%len(extern_name))
+
+    #write
+    configfile=open(config_path,'w+')
+    configfile.write('%d\n'%sig)
+    configfile.write("%d\n"%len(extern_name))
     for i in range(len(main_exaddr)):
-        f.write(hex(got_addr[i])[2:]+' '+hex(main_exaddr[i])[2:]+'\n')
-    f.write("%d\n"%len(patchfunc_addr))
+        configfile.write(hex(got_addr[i])[2:]+' '+hex(main_exaddr[i])[2:]+'\n')
+    configfile.write("%d\n"%len(patchfunc_addr))
     for i in range(len(patchfunc_addr)):
-        f.write(hex(mainfunc_addr[i])[2:]+' '+hex(patchfunc_addr[i])[2:]+'\n')
-    f.close()
+        configfile.write(hex(mainfunc_addr[i])[2:]+' '+hex(patchfunc_addr[i])[2:]+'\n')
+    configfile.close()
+
+def main(args):
+
+    # get parameters from args
+    main_path = args.main
+    patch_path = args.patch
+    target_path = args.object
+
+    if (main_path is None) or (target_path is None):
+        raise AttributeError
+
+    # create directory for the config and the tiger fix patch
+    fix_dir = os.path.join(os.path.dirname(target_path),"tigerfix")
+    if not os.path.isdir(fix_dir):
+        os.mkdir(fix_dir)
+
+    config_path = os.path.join(fix_dir, "config")
+    so_path = os.path.join(fix_dir, "patch.so")
+
+    # link objects
+    # todo: multiple patch files
+    do_link(patch_path,so_path)
+
+    # generating config file
+    gen_config(main_path,so_path,config_path)
+
+    # concat
+    cfg_bin = open(config_path, 'rb')
+    patch_bin = open(so_path, 'rb')
+    tfp_file = open(target_path, 'wb')
+    tfp_file.write(cfg_bin.read())
+    tfp_file.write(patch_bin.read())
+    tfp_file.close()
+
+    rmtree(fix_dir)
+
+# if __name__ == '__main__':
+#     main()
